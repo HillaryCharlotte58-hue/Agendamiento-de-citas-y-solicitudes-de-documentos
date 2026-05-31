e3abaf433bb713f320afffe931a6c19a8fa12e7b
Hillary Gil <hillarycharlotte58@gmail.com>
2026-05-31 15:09:51 -0500

    conectado a base implementada

---
+++ backend/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "Agendar_Citas";
+
+$conexion = mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $database
+);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+?>
